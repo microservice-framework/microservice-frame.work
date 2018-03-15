8129ca7151ae6d4fd67d6bdc6cbfd0859c0f19da
Kellen <root@goodroot.ca>
2018-03-15 13:10:16 -0700

    Out with old, in with new

--- scripts/deploy_after.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# cleanup & update links
-
-cd $DOCROOT
-
--- scripts/deploy_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Go to domain directory.
-cd $DOCROOT
-
-echo "Linking modules from $ZENCI_DEPLOY_DIR"
-ln -s $ZENCI_DEPLOY_DIR/* ./
