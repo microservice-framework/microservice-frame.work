73c0c2b01aa06cb160dcffc970f4aff280f05f4b
Gor Martsen <Gor.martsen@gmail.com>
2018-03-16 14:31:29 -0400

    fix deloy

--- scripts/deploy_after.sh
+++ scripts/deploy_after.sh
@@ -9,15 +9,15 @@ node pre-deploy.js
 
 if [ "$ROOTDIR" != "" ];then
   echo "Replacing root: $ROOTDIR"
-  sed -i 's|root:.*|root: '$ROOTDIR'|g' _config.yml
+  sed -i 's|root:.*$|root: '$ROOTDIR'|g' _config.yml
 fi
 
 echo "Replacing URL: $DOMAIN"
-sed -i 's|url:.* |url: https://'$DOMAIN'|g' _config.yml
+sed -i 's|url:.*$ |url: https://'$DOMAIN'|g' _config.yml
 
 echo "Replacing public_dir: $DOCROOT"
-sed -i 's|public_dir:.* |public_dir: '$DOCROOT'|g' _config.yml
+sed -i 's|public_dir:.*$ |public_dir: '$DOCROOT'|g' _config.yml
 
 
-hexo clean
-hexo generate
+./node_modules/.bin/hexo clean
+./node_modules/.bin/hexo generate
